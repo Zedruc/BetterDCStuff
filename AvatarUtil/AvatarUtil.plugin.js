40d98f8115f61855cd0258132a002c22c33f1b4e
Nofu <69540929+Zedruc@users.noreply.github.com>
2022-03-20 11:17:50 +0100

    Moved to newer plugin template

--- AvatarUtil/AvatarUtil.plugin.js
+++ AvatarUtil/AvatarUtil.plugin.js
@@ -5,22 +5,28 @@
  * @description Rightclick on a user's avatar on the profile to get different options for it
  */
 
-const downloadImage = (url, name) => {
-    fetch(url)
-        .then(resp => resp.blob())
-        .then(blob => {
-            const url = window.URL.createObjectURL(blob);
-            const a = document.createElement('a');
-            a.style.display = 'none';
-            a.href = url;
-            // the filename you want
-            a.download = name;
-            document.body.appendChild(a);
-            a.click();
-            window.URL.revokeObjectURL(url);
-        })
-        .catch(() => alert('An error sorry'));
-}
+const request = require('request');
+const fs = require('fs');
+const path = require('path');
+
+const config = {
+    info: {
+        name: 'AvatarUtil',
+        authors: [{
+            name: 'Zedruc',
+            discord_id: '568729687291985930',
+        }],
+        version: '1.0.0',
+        description: 'Rightclick on a user\'s avatar on the profile to get different options for it',
+        github: 'https://github.com/Zedruc/BetterDCStuff',
+        github_raw: 'https://raw.githubusercontent.com/Zedruc/BetterDCStuff/AvatarUtil.plugin.js',
+    }/* ,
+     changelog: [{
+         title: 'NEW',
+         type: 'added',
+         items: ['New Feature'],
+     }] */
+};
 
 const ctxMenu = (e, target) => {
     const userAvatarClass = ZeresPluginLibrary.WebpackModules.getByProps('avatar', 'badgeList', 'nameTag');
@@ -32,7 +38,7 @@ const ctxMenu = (e, target) => {
         else {
             // Context Menu with options
             var temp = avatarUrl.split(/(\d+)(?!.*\d)/)[0];
-            temp += "512";
+            temp += '512';
             avatarUrl = temp;
 
             const ctxMenu = ZeresPluginLibrary.ContextMenu.buildMenu([
@@ -53,7 +59,7 @@ const ctxMenu = (e, target) => {
                 {
                     id: 'download',
                     label: 'Download Avatar',
-                    action: () => { downloadImage(avatarUrl, 'avatar'); }
+                    action: () => { this.downloadImage(avatarUrl, 'avatar'); }
                 }
             ]);
             ZeresPluginLibrary.ContextMenu.openContextMenu(event, ctxMenu);
@@ -62,23 +68,58 @@ const ctxMenu = (e, target) => {
     } else return;
 }
 
-module.exports = class AvatarUtil {
-    start() {
-        if (!window.ZeresPluginLibrary) {
-            BdApi.shoConfiramtionModal('Missing Library', [
-                'To make AvatarUtil work please install ',
-                BdApi.React.createElement('a', { href: 'https://betterdiscord.app/plugin/ZeresPluginLibrary', target: "_blank" }, 'Zere\'s Plugin Library'),
-            ]);
-            return;
+module.exports = !global.ZeresPluginLibrary
+    ? class {
+        constructor() { this._config = config; }
+        load() {
+            BdApi.showConfirmationModal('Library plugin is needed', `The library plugin needed for ${config.info.name} is missing. Please click Download Now to install it.`, {
+                confirmText: 'Download',
+                cancelText: 'Cancel',
+                onConfirm: () => {
+                    request.get('https://rauenzi.github.io/BDPluginLibrary/release/0PluginLibrary.plugin.js', (error, response, body) => {
+                        if (error) return electron.shell.openExternal('https://betterdiscord.app/Download?id=9');
+                        fs.writeFileSync(path.join(BdApi.Plugins.folder, '0PluginLibrary.plugin.js'), body);
+                    });
+                }
+            }
+            );
         }
-
-        document.addEventListener('contextmenu', this.handle);
+        start() {
+            this.load();
+        }
+        stop() { }
     }
-    stop() { document.removeEventListener('contextmenu', this.handle); }
+    : (([Plugin, Library]) => {
+        const { PluginUpdater } = Library;
+        PluginUpdater.checkForUpdate(config.info.name, config.info.version, config.info.github_raw);
 
-    handle(event) {
-        if (!event.target.classList[0]) return;
-        if (event.target.classList[0].startsWith('avatar-')) ctxMenu(event, event.target);
-        else if (event.target.parentElement.parentElement?.classList[0].startsWith('avatar-')) ctxMenu(event, event.target.parentElement.parentElement);
-    }
-}
+        return class AvatarUtil extends Plugin {
+            onStart() {
+                document.addEventListener('contextmenu', this.handle);
+            }
+            onStop() { document.removeEventListener('contextmenu', this.handle); }
+
+            downloadImage(url, name) {
+                fetch(url)
+                    .then(resp => resp.blob())
+                    .then(blob => {
+                        const url = window.URL.createObjectURL(blob);
+                        const a = document.createElement('a');
+                        a.style.display = 'none';
+                        a.href = url;
+                        // the filename you want
+                        a.download = name;
+                        document.body.appendChild(a);
+                        a.click();
+                        window.URL.revokeObjectURL(url);
+                    })
+                    .catch(() => alert('An error sorry'));
+            }
+
+            handle(event) {
+                if (!event.target.classList[0]) return;
+                if (event.target.classList[0].startsWith('avatar-')) ctxMenu(event, event.target);
+                else if (event.target.parentElement.parentElement?.classList[0].startsWith('avatar-')) ctxMenu(event, event.target.parentElement.parentElement);
+            }
+        }
+    })(global.ZeresPluginLibrary.buildPlugin(config));
