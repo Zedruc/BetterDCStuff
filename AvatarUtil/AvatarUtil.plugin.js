35bc613f943f9217a66da6ea5c10788a746f8f72
Nofu <69540929+Zedruc@users.noreply.github.com>
2022-08-07 23:28:47 +0200

    Fixed the english

--- AvatarUtil/AvatarUtil.plugin.js
+++ AvatarUtil/AvatarUtil.plugin.js
@@ -2,7 +2,7 @@
  * @name AvatarUtil
  * @author Zedruc
  * @version 1.0.0
- * @description Allows you to download avatars, view them in your browser, and copy avatars' links
+ * @description Allows you to download avatars, view them in your browser, and copy avatar links
  * @source https://github.com/Zedruc/BetterDCStuff
  */
 
@@ -11,10 +11,6 @@ const fs = require('fs');
 const path = require('path');
 
 // webpack modules
-/**
- * Move to top so we only have to search twice
- * -> "Does a full module search on context menu"
- */
 var userAvatarClass;
 var avatarImgClass;
 var getUserById = BdApi.findModuleByProps('getUser').getUser;
@@ -22,35 +18,49 @@ var getUserById = BdApi.findModuleByProps('getUser').getUser;
 const config = {
     info: {
         name: 'AvatarUtil',
-        authors: [{
-            name: 'Zedruc',
-            discord_id: '568729687291985930',
-        }],
+        authors: [
+            {
+                name: 'Zedruc',
+                discord_id: '568729687291985930',
+            },
+        ],
         version: '1.0.0',
-        description: 'Allows you to download avatars, view them in your browser, and copy avatars\' links',
+        description:
+            'Allows you to download avatars, view them in your browser, and copy avatar links',
         github: 'https://github.com/Zedruc/BetterDCStuff',
-        github_raw: 'https://raw.githubusercontent.com/Zedruc/BetterDCStuff/main/AvatarUtil/AvatarUtil.plugin.js',
-    }/* ,
-         changelog: [{
-             title: 'NEW',
-             type: 'added',
-             items: ['New Feature'],
-         }] */
+        github_raw:
+            'https://raw.githubusercontent.com/Zedruc/BetterDCStuff/main/AvatarUtil/AvatarUtil.plugin.js',
+    },
+    changelog: [
+        {
+            title: 'Release',
+            type: 'added',
+            items: ['Changed the ways modules were loaded'],
+        },
+    ],
 };
 
 const ctxMenu = async (e, target) => {
     if (!userAvatarClass || !avatarImgClass) {
-        // Search for them a second time, now everything has to be loaded so we should be able to find the modules
-        // has to be done because searching them in the start method returns null/undefined
-        userAvatarClass = ZeresPluginLibrary.WebpackModules.getByProps('avatar', 'badgeList', 'nameTag');
-        avatarImgClass = ZeresPluginLibrary.WebpackModules.getByProps('avatar', 'status', 'svg');
+        userAvatarClass = ZeresPluginLibrary.WebpackModules.getByProps(
+            'avatar',
+            'badgeList',
+            'nameTag'
+        );
+        avatarImgClass = ZeresPluginLibrary.WebpackModules.getByProps(
+            'avatar',
+            'status',
+            'svg'
+        );
     }
-    if (!userAvatarClass || !avatarImgClass) return BdApi.showToast('Could not load modules', { type: 'error' });
+    if (!userAvatarClass || !avatarImgClass)
+        return BdApi.showToast('Could not load modules', { type: 'error' });
     if (target?.classList?.contains(userAvatarClass.avatar)) {
-
         var userId = target.getAttribute('data-user-id');
         const user = await getUserById(userId);
-        var avatarUrl = target.querySelector(`.${avatarImgClass.avatar}`).getAttribute('src');
+        var avatarUrl = target
+            .querySelector(`.${avatarImgClass.avatar}`)
+            .getAttribute('src');
         const downloadName = `avatar-${user.username}`;
 
         // Context Menu with options
@@ -64,29 +74,31 @@ const ctxMenu = async (e, target) => {
                 label: 'Copy URL to Clipboard',
                 action: () => {
                     require('electron').clipboard.writeText(avatarUrl);
-                }
+                },
             },
             {
                 id: 'open',
                 label: 'Open in Browser',
                 action: () => {
                     require('electron').shell.openExternal(avatarUrl);
-                }
+                },
             },
             {
                 id: 'download',
                 label: 'Download Avatar',
-                action: () => { downloadImage(avatarUrl, downloadName); }
-            }
+                action: () => {
+                    downloadImage(avatarUrl, downloadName);
+                },
+            },
         ]);
         ZeresPluginLibrary.ContextMenu.openContextMenu(event, ctxMenu);
     } else return;
-}
+};
 
 const downloadImage = (url, name) => {
     fetch(url)
-        .then(resp => resp.blob())
-        .then(blob => {
+        .then((resp) => resp.blob())
+        .then((blob) => {
             const url = window.URL.createObjectURL(blob);
             const a = document.createElement('a');
             a.style.display = 'none';
@@ -97,50 +109,85 @@ const downloadImage = (url, name) => {
             a.click();
             window.URL.revokeObjectURL(url);
         })
-        .catch(() => BdApi.showToast('An error occured trying to download the image', { type: 'error' }));
-}
+        .catch(() =>
+            BdApi.showToast('An error occured trying to download the image', {
+                type: 'error',
+            })
+        );
+};
 
 module.exports = !global.ZeresPluginLibrary
     ? class {
-        constructor() { this._config = config; }
-        load() {
-            BdApi.showConfirmationModal('Library plugin is needed', `The library plugin needed for ${config.info.name} is missing. Please click Download Now to install it.`, {
-                confirmText: 'Download',
-                cancelText: 'Cancel',
-                onConfirm: () => {
-                    request.get('https://rauenzi.github.io/BDPluginLibrary/release/0PluginLibrary.plugin.js', (error, response, body) => {
-                        if (error) return electron.shell.openExternal('https://betterdiscord.app/Download?id=9');
-                        fs.writeFileSync(path.join(BdApi.Plugins.folder, '0PluginLibrary.plugin.js'), body);
-                    });
-                }
-            });
-        }
-        start() {
-            this.load();
-        }
-        stop() { }
-    }
+          constructor() {
+              this._config = config;
+          }
+          load() {
+              BdApi.showConfirmationModal(
+                  'Library plugin is needed',
+                  `The library plugin needed for ${config.info.name} is missing. Please click Download Now to install it.`,
+                  {
+                      confirmText: 'Download',
+                      cancelText: 'Cancel',
+                      onConfirm: () => {
+                          request.get(
+                              'https://rauenzi.github.io/BDPluginLibrary/release/0PluginLibrary.plugin.js',
+                              (error, response, body) => {
+                                  if (error)
+                                      return electron.shell.openExternal(
+                                          'https://betterdiscord.app/Download?id=9'
+                                      );
+                                  fs.writeFileSync(
+                                      path.join(
+                                          BdApi.Plugins.folder,
+                                          '0PluginLibrary.plugin.js'
+                                      ),
+                                      body
+                                  );
+                              }
+                          );
+                      },
+                  }
+              );
+          }
+          start() {
+              this.load();
+          }
+          stop() {}
+      }
     : (([Plugin, Library]) => {
-        /**
-         * removed update line because the library does it automatically
-         * -> "attempts to check for update on construction/execution, the library auto checks for update anyways"
-         */
-        const { WebpackModules } = Library;
+          const { WebpackModules } = Library;
 
-        return class AvatarUtil extends Plugin {
-            onStart() {
-                // search webpack modules once
-                userAvatarClass = WebpackModules.getByProps('avatar', 'badgeList', 'nameTag');
-                avatarImgClass = WebpackModules.getByProps('avatar', 'status', 'svg'); ZeresPluginLibrary
+          return class AvatarUtil extends Plugin {
+              onStart() {
+                  // search webpack modules once
+                  userAvatarClass = WebpackModules.getByProps(
+                      'avatar',
+                      'badgeList',
+                      'nameTag'
+                  );
+                  avatarImgClass = WebpackModules.getByProps(
+                      'avatar',
+                      'status',
+                      'svg'
+                  );
+                  ZeresPluginLibrary;
 
-                document.addEventListener('contextmenu', this.handle);
-            }
-            onStop() { document.removeEventListener('contextmenu', this.handle); }
+                  document.addEventListener('contextmenu', this.handle);
+              }
+              onStop() {
+                  document.removeEventListener('contextmenu', this.handle);
+              }
 
-            handle(event) {
-                if (!event.target.classList[0]) return;
-                if (event.target.classList[0].startsWith('avatar-')) ctxMenu(event, event.target);
-                else if (event.target.parentElement.parentElement?.classList[0].startsWith('avatar-')) ctxMenu(event, event.target.parentElement.parentElement);
-            }
-        }
-    })(global.ZeresPluginLibrary.buildPlugin(config));
+              handle(event) {
+                  if (!event.target.classList[0]) return;
+                  if (event.target.classList[0].startsWith('avatar-'))
+                      ctxMenu(event, event.target);
+                  else if (
+                      event.target.parentElement.parentElement?.classList[0].startsWith(
+                          'avatar-'
+                      )
+                  )
+                      ctxMenu(event, event.target.parentElement.parentElement);
+              }
+          };
+      })(global.ZeresPluginLibrary.buildPlugin(config));
