41a63e2bed2bb4f0f8d8a288c65e713a803ea683
Zedruc <69540929+Zedruc@users.noreply.github.com>
2023-02-19 17:41:59 +0100

    Private methods, add `Hotkey` modifier

--- _dev/keybindHandler.js
+++ _dev/keybindHandler.js
@@ -1,65 +1,97 @@
 /**
- * Omly starts listening once `init` was called
+ * Only starts listening once `init` was called
  */
 
 class KeybindHandler {
+  #_listeners = [];
+  #_activeKeys = [];
+  #hotkeys = [];
   /**
-   * @param {Array.<{codes: Number[], callback: (hotkeyObject: Object) => {}>} keybinds Array of objects representing hotkeys
+   * @param {Array.<{name: String, codes: Number[], callback: (hotkeyObject: Object) => {}>=} keybinds Array of objects representing hotkeys
    */
   constructor(keybinds) {
-    this._listeners = [];
-    this._activeKeys = [];
-    this.hotkeys = [];
-  }
-
-  keyUp(event) {
-    this.removeActiveKey(event.keyCode);
-  }
-
-  keyDown(event) {
-    // esc to close notepad
-    if (event.keyCode == 27) this.closeNotepad();
-    if (this._activeKeys.includes(event.keyCode)) return;
-    this._activeKeys.push(event.keyCode);
-    let hotkeyCheck = this.checkHotkey();
-    if (hotkeyCheck) this._emit(hotkeyCheck);
-  }
-
-  removeActiveKey(keyCode) {
-    if (this._activeKeys.includes(undefined))
-      this._activeKeys.splice(this._activeKeys.indexOf(undefined), 1);
-    this._activeKeys.splice(this._activeKeys.indexOf(keyCode), 1);
+    if (keybinds?.length) {
+      for (let i = 0; i < keybinds.length; i++) {
+        const keybind = keybinds[i];
+        this.addHotkey(keybind.name, keybind.codes, keybind.callback);
+      }
+    }
   }
 
   /**
-   *
+   * @param {string} name Name of the keybind
    * @param {array} codes Array of keycodes
    * @param {function({ codes: Number[], cb: Function, index: Number })} listener Hotkey trigger callback
+   * @returns {{name: string, codes: number[], cb: Function, index: number, modifiyHotkey: Function(name: string, codes: number[])}}
    */
-  addHotkey(codes, listener) {
+  addHotkey(name, codes, listener) {
     if (!codes.length)
       throw new Error('Must provide an array of keyCodes to act as hotkey definition');
-    this.hotkeys.push({ codes: codes, cb: listener, index: this.hotkeys.length });
-  }
-
-  /**
-   * Returns the hotkey which was triggered or false
-   */
-  checkHotkey() {
-    for (let i = 0; i < this.hotkeys.length; i++) {
-      const hotkey = this.hotkeys[i].codes.sort().toString();
-      let active = this._activeKeys.sort().toString();
-      if (active === hotkey) return this.hotkeys[i];
+    for (let i = 0; i < this.#hotkeys.length; i++) {
+      const hotkey = this.#hotkeys[i];
+      if (hotkey.name === name) throw new Error(`Hotkey with name "${name}" already exists`);
     }
-    return false;
+    const keybind = {
+      name: name,
+      codes: codes,
+      cb: listener,
+      index: this.#hotkeys.length,
+      modifiyKeycodes: this.#_modifyHotkey.bind(
+        Object.assign(this, { index: this.#hotkeys.length })
+      ),
+    };
+    this.#hotkeys.push(keybind);
+    return keybind;
   }
 
   /**
    * The keybind handler only starts listening once it was initialised
    */
   init() {
-    this._registerListener('keydown', this.keyDown);
-    this._registerListener('keyup', this.keyUp);
+    this.#_registerListener('keydown', this.#_keyDown);
+    this.#_registerListener('keyup', this.#_keyUp);
+  }
+
+  /**
+   * Searches for a hotkey by name. Returns undefined if none is found
+   * @param {string} name - Name of the keybind
+   * @returns {(Hotkey|undefined)}
+   */
+  getKeybindByName(name) {
+    for (let i = 0; i < this.#hotkeys.length; i++) {
+      const hotkey = this.#hotkeys[i];
+      if (hotkey.name === name) return hotkey;
+    }
+    return null;
+  }
+
+  #_keyUp(event) {
+    this.#_removeActiveKey(event.keyCode);
+  }
+
+  #_keyDown(event) {
+    if (this.#_activeKeys.includes(event.keyCode)) return;
+    this.#_activeKeys.push(event.keyCode);
+    let hotkeyCheck = this.#_checkHotkey();
+    if (hotkeyCheck) this.#_emit(hotkeyCheck);
+  }
+
+  #_removeActiveKey(keyCode) {
+    if (this.#_activeKeys.includes(undefined))
+      this.#_activeKeys.splice(this.#_activeKeys.indexOf(undefined), 1);
+    this.#_activeKeys.splice(this.#_activeKeys.indexOf(keyCode), 1);
+  }
+
+  /**
+   * Returns the hotkey which was triggered or false
+   */
+  #_checkHotkey() {
+    for (let i = 0; i < this.#hotkeys.length; i++) {
+      const hotkey = this.#hotkeys[i].codes.sort().toString();
+      let active = this.#_activeKeys.sort().toString();
+      if (active === hotkey) return this.#hotkeys[i];
+    }
+    return false;
   }
 
   /**
@@ -67,27 +99,64 @@ class KeybindHandler {
    * @param {string} eventName
    * @param {function} fn
    */
-  _registerListener(eventName, fn, target = window) {
+  #_registerListener(eventName, fn, target = window) {
     fn = fn.bind(this);
     target.addEventListener(eventName, fn);
-    this._listeners.push({ name: eventName, functionRef: fn, target: target });
+    this.#_listeners.push({ name: eventName, functionRef: fn, target: target });
   }
 
-  _removeAllListeners() {
-    for (let i = 0; i < this._listeners.length; i++) {
-      const { name, target, functionRef } = this.listeners[i];
-      target.removeEventListener(name, functionRef);
-    }
-    this._listeners = [];
-  }
-
-  _emit(hotkey) {
+  #_emit(hotkey) {
     hotkey.cb(hotkey);
+  }
+
+  /**
+   * Modifies a hotkey's keycodes
+   * @param {number[]} newCodes - New keycodes for the hotkey
+   * @returns New hotkey object
+   */
+  #_modifyHotkey(newCodes) {
+    console.log(this);
+    let hotkeyToModify = this.#hotkeys[this.index];
+    if (!hotkeyToModify) throw new Error(`Hotkey with index ${index} could not be found`);
+    if (!newCodes?.length)
+      throw new Error('Must define a new array of keycodes to assign to the hotkey');
+
+    hotkeyToModify.codes = newCodes;
+    this.#hotkeys[this.index] = hotkeyToModify;
+    return this.#hotkeys[this.index];
   }
 }
 
-const kbh = new KeybindHandler();
-kbh.addHotkey([17, 76], hotkey => {
-  console.log('ctrl+l triggered:\n', hotkey);
-});
+const keybinds = [
+  {
+    name: 'ctrl+l',
+    codes: [17, 76],
+    callback: hotkey => {
+      console.log('ctrl+l triggered:\n', hotkey);
+    },
+  },
+  {
+    name: 'ctrl+k',
+    codes: [17, 75],
+    callback: hotkey => {
+      console.log('ctrl+k triggered:\n', hotkey);
+    },
+  },
+  {
+    name: 'ctrl+j',
+    codes: [17, 74],
+    callback: hotkey => {
+      console.log('ctrl+j triggered:\n', hotkey);
+    },
+  },
+  {
+    name: 'ctrl+h',
+    codes: [17, 72],
+    callback: hotkey => {
+      console.log('ctrl+h triggered:\n', hotkey);
+    },
+  },
+];
+
+const kbh = new KeybindHandler(keybinds);
 kbh.init();
